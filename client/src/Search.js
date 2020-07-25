0044da89b3c1e5dbc63fac08f38c772c0adcfdfd
Brad Schaefer <bradrschaefer411@gmail.com>
2020-07-25 09:20:08 -0400

    Upgrade

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
 function Search(props) {
   const [searchterm, setsearchterm] = useState();
+    const [results, setresults] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("searchterm is " + searchterm);
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + searchterm)
+      .then((res) => setresults(res.data.items))
+      .catch((err) => console.log(err));
   };
   return (
     <div className="App">
@@ -18,6 +23,19 @@ function Search(props) {
         onChange={(e) => setsearchterm(e.target.value)}
       />
       <button onClick = {handleSubmit}>Search!</button>
+      {results.map(function (book){
+        console.log(book);
+        return (
+        <div>
+        <h1>{book.volumeInfo.title}</h1>
+        <h2>{book.volumeInfo.authors}</h2>
+        <h6>{book.volumeInfo.description}</h6>
+        <h4><img src={book.volumeInfo.imageLinks.thumbnail}></img></h4>
+        <a href={book.volumeInfo.infoLink}>Click here to view</a>
+        <hr></hr>
+        </div>
+        )
+      })}
     </div>
   );
 }
